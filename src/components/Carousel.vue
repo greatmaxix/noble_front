2030c0b3008ced04529e231eb94b5cdf4b72eea7
greatmaxix <greatmaxix@gmail.com>
2024-06-02 20:00:58 +0500

    Latest fixes

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="h-[300px] md:h-[400px]">
-    <Transition name="link-transition" mode="out-in">
-      <router-link
-        v-if="currentProduct"
-        class="relative image-container cursor-pointer"
-        :to="`/product/${currentProduct.id}/${currentProduct.type}`"
-      >
-        <img
-          class="fit-image"
-          :src="getImgUrl(currentProduct.image)"
-          :alt="currentProduct.name"
-        />
-        <div class="absolute bottom-0 w-full">
-          <div class="mb-10 flex justify-between">
-            <h2
-              class="self-end p-5 capitalize text-3xl w-full bg-gray-950 opacity-80 font-thin text-white text-center text-2xl lg:text-4xl"
-            >
-              {{ currentProduct.name }}
-            </h2>
+  <Transition name="link-transition" mode="out-in">
+    <div>
+      <div class="h-[300px] md:h-[400px]">
+        <router-link v-if="currentProduct" class="relative image-container cursor-pointer"
+          :to="`/product/${currentProduct.id}/${currentProduct.type}`">
+          <div class="relative w-full h-full">
+            <img class="fit-image" :src="getImgUrl(currentProduct.image)" :alt="currentProduct.name" />
+            <div class="hidden xl:block absolute bottom-0 w-full">
+              <div class="mb-10 flex justify-between">
+                <h2
+                  class="self-end p-5 capitalize text-3xl w-full bg-gray-950 opacity-80 font-thin text-white text-center text-2xl lg:text-4xl">
+                  {{ currentProduct.name }}
+                </h2>
+              </div>
+            </div>
           </div>
-        </div>
-      </router-link>
-    </Transition>
-  </div>
+        </router-link>
+      </div>
+      <div class="xl:hidden">
+        <router-link v-if="currentProduct" class="relative image-container cursor-pointer"
+          :to="`/product/${currentProduct.id}/${currentProduct.type}`">
+          <h2
+            class="self-end p-5 capitalize text-3xl w-full bg-gray-950 opacity-80 font-thin text-white text-center text-2xl lg:text-4xl">
+            {{ currentProduct.name }}
+          </h2>
+        </router-link>
+      </div>
+    </div>
+  </Transition>
 </template>
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
